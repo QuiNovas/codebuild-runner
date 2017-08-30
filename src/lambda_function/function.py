74e6e80f1e8dead0eaf49674d45559edb9d097e9
Joseph Wortmann <joseph.wortmann@gmail.com>
2017-08-29 21:00:43 -0500

    added logging

--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -11,8 +11,11 @@ def handler(event, context):
 
     code_build = boto3.client('codebuild', config=Config(signature_version='s3v4'))
 
+    logger.info('Starting CodeBuild project {}'.format(os.environ['PROJECT_NAME']))
     code_build.start_build(
         projectName=os.environ['PROJECT_NAME']
     )
+    logger.info('Started CodeBuild project {}'.format(os.environ['PROJECT_NAME']))
+
 
     return event
